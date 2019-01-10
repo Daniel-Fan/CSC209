f2bf9c37b5f65fb09df2826e074c39f899b217bf
daniel_fan <fanyuchensx@gmail.com>
2019-01-10 14:45:28 -0500

    submit

---
+++ lab1/compile_all.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+gcc -g -Wall -std=gnu99 -o count count.c
+gcc -g -Wall -std=gnu99 -o echo_arg echo_arg.c
+gcc -g -Wall -std=gnu99 -o echo_stdin echo_stdin.c
+gcc -g -Wall -std=gnu99 -o hello hello.c
+./count 10
+./echo_arg 10
+./echo_stdin
+./hello
